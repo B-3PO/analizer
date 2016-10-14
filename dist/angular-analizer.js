fac9a7ac7a949f9b8ad090f2eb1ee33a33c5eb90
Ben Rubin <benrubin@Bens-MacBook-Pro-3.local>
2016-10-14 00:36:15 -0500

    new version

--- dist/angular-analizer.js
+++ dist/angular-analizer.js
@@ -1,96 +1,6 @@
 (function(){"use strict";angular.module('analizer', []);
 }());
 (function(){"use strict";
-configGa.$inject = ["$analizerProvider"];angular
-  .module('analizer.ga', ['analizer'])
-  .config(configGa);
-
-
-/*@ngInject*/
-function configGa($analizerProvider) {
-  var dispatcher = getDispatcher();
-  var demensions = [];
-
-  $analizerProvider.on('url', function (data) {
-    if ($analizerProvider.trackPageViews === false) { return; }
-    // set page for all calls
-    dispatcher({
-      command: 'set',
-      page: data.relative
-    });
-
-    // pageview event
-    dispatcher({
-      hitType: 'pageview',
-      page: data.relative
-    });
-  });
-
-  $analizerProvider.on('click', function (data) {
-    dispatcher({
-      hitType: 'event',
-      eventCategory: data.type,
-      eventAction: data.action,
-      eventLabel: data.label
-    });
-  });
-
-  $analizerProvider.on('onComplete', function (data) {
-    dispatcher({
-      hitType: 'event',
-      eventCategory: data.type,
-      eventAction: data.action,
-      eventLabel: data.label
-    });
-  });
-
-  $analizerProvider.on('onCompleteError', function (data) {
-    dispatcher({
-      hitType: 'event',
-      eventCategory: data.type,
-      eventAction: data.action,
-      eventLabel: data.label
-    });
-  });
-
-  $analizerProvider.on('setCustomData', function (data) {
-    var obj = {};
-    obj[data.name] = data.value.toString();
-    dispatcher(angular.extend({command: 'set'}, obj));
-  });
-
-
-  $analizerProvider.on('setUser', function (user) {
-    dispatcher(angular.extend({command: 'set'}, {
-      userId: user.toString()
-    }));
-  });
-
-  $analizerProvider.on('timing', function (data) {
-    dispatcher({
-      hitType: 'timing',
-      timingCategory: data.category,
-      timingVar: data.label,
-      timingValue: data.milliseconds
-    });
-  });
-}
-
-
-function getDispatcher() {
-  if (typeof ga !== 'function') {
-    void 0;
-    return angular.noop;
-  }
-
-  return function(config) {
-    var command = config.command || 'send';
-    delete config.command;
-    ga(command, config);
-  };
-}
-}());
-(function(){"use strict";
 anailzerRun.$inject = ["$analizer", "$rootScope"];angular
   .module('analizer')
   .provider('$analizer', analizerProvider)
@@ -191,7 +101,8 @@ function analizerProvider() {
       emit: provider.emit,
       trackPageViews: provider.trackPageViews,
       setCustomData: provider.setCustomData,
-      setUser: provider.setUser
+      setUser: provider.setUser,
+      sendTiming: provider.sendTiming
     };
     return service;
 
@@ -469,5 +380,95 @@ function analizerDirective($analizerUtil, $analizer, $parse, $q, $rootScope) {
       }
     };
   }
+}
+}());
+(function(){"use strict";
+configGa.$inject = ["$analizerProvider"];angular
+  .module('analizer.ga', ['analizer'])
+  .config(configGa);
+
+
+/*@ngInject*/
+function configGa($analizerProvider) {
+  var dispatcher = getDispatcher();
+  var demensions = [];
+
+  $analizerProvider.on('url', function (data) {
+    if ($analizerProvider.trackPageViews === false) { return; }
+    // set page for all calls
+    dispatcher({
+      command: 'set',
+      page: data.relative
+    });
+
+    // pageview event
+    dispatcher({
+      hitType: 'pageview',
+      page: data.relative
+    });
+  });
+
+  $analizerProvider.on('click', function (data) {
+    dispatcher({
+      hitType: 'event',
+      eventCategory: data.type,
+      eventAction: data.action,
+      eventLabel: data.label
+    });
+  });
+
+  $analizerProvider.on('onComplete', function (data) {
+    dispatcher({
+      hitType: 'event',
+      eventCategory: data.type,
+      eventAction: data.action,
+      eventLabel: data.label
+    });
+  });
+
+  $analizerProvider.on('onCompleteError', function (data) {
+    dispatcher({
+      hitType: 'event',
+      eventCategory: data.type,
+      eventAction: data.action,
+      eventLabel: data.label
+    });
+  });
+
+  $analizerProvider.on('setCustomData', function (data) {
+    var obj = {};
+    obj[data.name] = data.value.toString();
+    dispatcher(angular.extend({command: 'set'}, obj));
+  });
+
+
+  $analizerProvider.on('setUser', function (user) {
+    dispatcher(angular.extend({command: 'set'}, {
+      userId: user.toString()
+    }));
+  });
+
+  $analizerProvider.on('timing', function (data) {
+    dispatcher({
+      hitType: 'timing',
+      timingCategory: data.category,
+      timingVar: data.label,
+      timingValue: data.milliseconds
+    });
+  });
+}
+
+
+function getDispatcher() {
+  if (typeof ga !== 'function') {
+    void 0;
+    return angular.noop;
+  }
+
+  return function(config) {
+    var command = config.command || 'send';
+    delete config.command;
+    ga(command, config);
+  };
 }
 }());
